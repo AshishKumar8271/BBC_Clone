efd07e9bd6d0151c658af744449779a781ce6d0d
AshishKumar8271 <ashishgujjar277@gmail.com>
2024-06-25 16:20:27 +0530

    some changes

--- src/Features/NewsSlice.ts
+++ src/Features/NewsSlice.ts
@@ -20,7 +20,7 @@ const initialState:NewsState = {
 }
 
 export const fetchNewsData = createAsyncThunk("user/fetchNewsData",async (menu:string) => {
-    const res = await fetch(`https://newsapi.org/v2/everything?q=${menu}&pageSize=100&apiKey=1d23c25bef164d55a4cc39709db1d27b`);
+    const res = await fetch(`https://newsapi.org/v2/everything?q=${menu}&pageSize=100&apiKey=${import.meta.env.VITE_News_API_KEY}`);
     if(!res.ok) {
         throw new Error("Network Response was not ok");
     }
